6d4ff447083a53d64da4b88ba0f3ee2427c03965
yair <yabartf@gmail.com>
2020-11-26 16:20:48 +0200

    Calender

--- app/src/main/java/com/example/android_project_5840_6639/UI/AddTravelActivity.java
+++ app/src/main/java/com/example/android_project_5840_6639/UI/AddTravelActivity.java
@@ -8,15 +8,27 @@ import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android_project_5840_6639.Entities.Travel;
 import com.example.android_project_5840_6639.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AddTravelActivity extends AppCompatActivity {
     private final Travel travel = new Travel();
-
+    private Date startDate;
+    private Date endDate;
+    private Boolean choosed = false;
+    static int addTravelIndex = 0;
+    int numOfDestnation = 10;
     TravelViewModel travelViewModel;
 
     @Override
@@ -26,11 +38,78 @@ public class AddTravelActivity extends AppCompatActivity {
         travelViewModel = new ViewModelProvider(this).get(TravelViewModel.class);
         travelViewModel.getIsSuccess().observe(this, aBoolean ->
                 Toast.makeText(getBaseContext(),"updated",Toast.LENGTH_LONG).show());
+        CalendarView calendar = findViewById(R.id.calendarDate);
+        Date now = new Date();
+        calendar.setMinDate(now.getTime());
+        calendar.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            if (!choosed){
+                Toast.makeText(getBaseContext(),"choosed",Toast.LENGTH_LONG).show();
+                String tempDate = "" + year +"/"  + (++month) + "/" + dayOfMonth +" 00:00:00";
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                try {
+                    startDate = sdf.parse(tempDate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                //calendar.setMinDate(startDate.getTime() + 86400001);
+                choosed = true;
+            }
+            else
+            if (choosed)
+            {
+                String tempDate = "" + year +"/" + (++month) + "/" + dayOfMonth +" 00:00:00";
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                try {
+                    endDate = sdf.parse(tempDate);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                if (endDate.getTime() < startDate.getTime()) {
+                    Toast.makeText(getBaseContext(), "Error", Toast.LENGTH_LONG).show();
+                    endDate = null;
+                }
+                else
+                    calendar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
+
 
     public void addAddress(View view) {
+        EditText editText = new EditText(this);
+        Button remuveButton = new Button(this);
+        LinearLayout linearLayout = findViewById(R.id.addTravelsLayout);
+        editText.setHeight(288);
+        editText.setWidth(49);
+        editText.setEms(10);
+        editText.setId(addTravelIndex + numOfDestnation);
+        editText.setHint("Destination address");
+        remuveButton.setHeight(50);
+        remuveButton.setWidth(288);
+        remuveButton.setEms(10);
+        remuveButton.setId(addTravelIndex++);
+        String text = "Remove";
+        remuveButton.setText(text);
+        linearLayout.addView(editText,linearLayout.getChildCount());
+        linearLayout.addView(remuveButton,linearLayout.getChildCount());
 
+
+        remuveButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+            public void onClick(View v) {
+            int id = v.getId();
+            linearLayout.removeView(v);
+            TextView tv = findViewById(id + numOfDestnation);
+            linearLayout.removeView(tv);
+            for (int i = 2;i < linearLayout.getChildCount() / 2 +1;i++){
+                linearLayout.getChildAt(i).setId(i - 2);
+                linearLayout.getChildAt(i + numOfDestnation).setId(i + numOfDestnation -2);
+            }
+            --addTravelIndex;
+            }
+        });
     }
+
 
 
     public void submitted(View view) {
